eff64fa64f1a0b13b8c817b8fe1629ada568b0cd
Mark Thomas <mark@feed-london.com>
2017-05-26 07:46:04 +0100

    added disabled prop tests

--- src/Checkbox/Checkbox.test.js
+++ src/Checkbox/Checkbox.test.js
@@ -8,7 +8,7 @@ it('Checkbox renders without crashing', () => {
   ReactDOM.render(<Checkbox />, div);
 });
 
-test('Checkbox changes the text after click', () => {
+test('Checkbox changes internal span text after click', () => {
 
   const checkbox = shallow(
     <Checkbox />
@@ -17,6 +17,17 @@ test('Checkbox changes the text after click', () => {
   expect(checkbox.find('span').text()).toEqual("");
   checkbox.find('label').simulate('click');
   expect(checkbox.find('span').text()).toEqual('✓');
+});
+
+test('If set to disabled, Checkbox does not change internal span text after click', () => {
+
+  const checkbox = shallow(
+    <Checkbox disabled={true}/>
+  );
+
+  expect(checkbox.find('span').text()).toEqual("");
+  checkbox.find('label').simulate('click');
+  expect(checkbox.find('span').text()).toEqual("");
 });
 
 test('Checkbox prop data-checked changes after click', () => {
@@ -29,6 +40,18 @@ test('Checkbox prop data-checked changes after click', () => {
   checkbox.find('div').simulate('click');
   expect(checkbox.find('div').node.props['data-checked']).toEqual(true);
 });
+
+test('If set to disabled, Checkbox prop data-checked does not change after click', () => {
+
+  const checkbox = shallow(
+    <Checkbox disabled={true}/>
+  );
+
+  expect(checkbox.find('div').node.props['data-checked']).toEqual(false);
+  checkbox.find('div').simulate('click');
+  expect(checkbox.find('div').node.props['data-checked']).toEqual(false);
+});
+
 
 test('Checkbox wrapping label receives modified className from props', () => {
 
